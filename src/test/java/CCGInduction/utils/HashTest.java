d9a421d819dfabdbe4fdb812137dffda35a63bd2
ybisk <ybisk@yo-tech.com>
2015-12-05 12:38:40 -0800

    Bad directory paths

--- src/test/java/CCGInduction/utils/HashTest.java
+++ src/test/java/CCGInduction/utils/HashTest.java
@@ -9,7 +9,7 @@ public class HashTest extends TestCase {
 
   public void testHash() throws Exception {
     // Checks for hash conflicts
-    BufferedReader reader = TextFile.Reader("src/test/java/edu/illinois/cs/nlp/hdpccg/utils/vocab.txt.gz");
+    BufferedReader reader = TextFile.Reader("src/test/java/CCGInduction/utils/vocab.txt.gz");
     HashMap<Long,String> strings = new HashMap<>();
     String line;
     while ((line = reader.readLine()) != null) {
@@ -19,7 +19,7 @@ public class HashTest extends TestCase {
     }
     System.out.println("Successfully hashed: " + strings.size() + " CCGbank items");
 
-    reader = TextFile.Reader("src/test/java/edu/illinois/cs/nlp/hdpccg/utils/BLIIP.txt.gz");
+    reader = TextFile.Reader("src/test/java/CCGInduction/utils/BLIIP.txt.gz");
     while ((line = reader.readLine()) != null) {
       long hash = Hash.hash(line.trim().toLowerCase());
       assertFalse("Conflict: " + hash + '\t' + strings.get(hash) + '\t' + line, strings.containsKey(hash) && !strings.get(hash).equals(line.trim().toLowerCase()));
